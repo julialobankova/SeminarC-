8d06654976800a52c19b20c949ff02a20eabdbef
Julechka <julialobankova123@gmail.com>
2022-07-08 12:07:08 +0300

    Zadacha4

---
+++ lesson1z4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = int.Parse(Console.ReadLine());
+int i = 0;
+int temp = 0;
+while (i < a * 2 + 1)
+{
+ Console.WriteLine(-a + temp);
+ i++;
+ temp++;
+}
+
+
+
+//Console.WriteLine("Write a number a: ");
+//int a = int.Parse(Console.ReadLine());
+// if (a<0)
+//int i=-a;
+//while (i<=a)
+//{
+ //   Console.WriteLine(i);
+ //   i++;
+//}
