7c5acbacc2c044bf13648f6ab3374fe6d12861b3
Julechka <julialobankova123@gmail.com>
2022-07-08 11:07:19 +0300

    Zadacha1

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write a number: ");
+string s_a = Console.ReadLine();
+
+int a = int.Parse(s_a);
+Console.WriteLine(a * a);
