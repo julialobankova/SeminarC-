9b07935c8f84ac69afefd1217478b9624efa1323
Julechka <julialobankova123@gmail.com>
2022-07-12 01:26:42 +0300

    zadacha4

---
+++ Homework/T4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.*/
+
+ Console.WriteLine("Введите число и я покажу все четные числа до вашего числа: ");
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+if (i > a)
+{
+    Console.WriteLine(a-1);
+}
+while(i<=a)
+{
+    Console.WriteLine(i);
+    i=i+2;
+}
+
