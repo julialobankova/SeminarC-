48027201f201e92edcabe144cc7d78b1d2fa94d7
Julechka <julialobankova123@gmail.com>
2022-07-16 19:26:52 +0300

    42hhh

---
+++ lesson2/t4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая принимает на вход
+число и проверяет, кратно ли оно одновременно 7 и
+23.*/
+
+void Krat(int number)
+{
+
+if (number % 7 == 0 && number % 23 == 0)
+    Console.WriteLine("Молоде классное число");
+else
+Console.WriteLine("no");
+}
+
+int a = int.Parse(Console.ReadLine());
+Krat(a);
+
