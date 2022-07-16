8aedcfcf63b527c7b23aedd4011910ed53179b6b
Julechka <julialobankova123@gmail.com>
2022-07-16 19:54:07 +0300

    первая задача

---
+++ Homework/hwsem2/t1/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+*/
+int MT1 (int number)
+{
+    return number % 100 /10; 
+}
+Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(MT1(a));
