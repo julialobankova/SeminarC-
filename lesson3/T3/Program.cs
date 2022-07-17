febcd1b1ff4bccf37aa7a7d706fb8c14350cb040
Julechka <julialobankova123@gmail.com>
2022-07-17 17:35:12 +0300

    4

---
+++ lesson3/T3/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 2: Напишите программу, которая
+принимает на вход координаты двух точек и
+находит расстояние между ними в 2D
+пространстве.*/
+
+double distance (double x1, double y1, double x2, double y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)),3);
+}
+
+Console.WriteLine(distance(2,6,3,1));
---
+++ lesson3/t2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая по
+заданному номеру четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y).*/
+
+void Quarters(int quart)
+{
+    if (quart > 4 || quart < 1)
+        Console.WriteLine(" No");
+    else if (quart == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (quart == 2)
+        Console.WriteLine("x < 0 y > 0");
+    else if (quart == 3)
+        Console.WriteLine("x < 0 y < 0");
+    else if (quart == 4)
+        Console.WriteLine("x > 0 y < 0");
+
+}
+
+Quarters(1);
+Quarters(0);
+Quarters(5);
+Quarters(2);
+Quarters(3);
+Quarters(-5);
