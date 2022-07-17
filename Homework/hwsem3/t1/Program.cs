8a65f5b2b226e2c5b1ad113dff7138639fe06c12
Julechka <julialobankova123@gmail.com>
2022-07-17 22:18:59 +0300

    dz3

---
+++ Homework/hwsem3/t1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает
+ на вход пятизначное число и проверяет, 
+является ли оно палиндромом.*/
+
+void Pali(int number)
+{
+    if (number > 100000 || number < 10000 )
+    {
+        Console.WriteLine("Error");
+    }
+    
+    else if (number / 10000 == number % 10 && number % 10000 / 1000 == number % 100 / 10)
+    {
+        Console.WriteLine("Число палиндром");
+    }
+
+    else
+    {
+        Console.WriteLine("Число не палиндром");       
+    }
+}
+
+Pali(24542);
+Pali(255);
+Pali(3699874);
+Pali (56987);
+
+
+
+
+
+
