d2e2004f63045dda9bb1d03d70a926b04ee0ef57
Julechka <julialobankova123@gmail.com>
2022-07-17 16:28:49 +0300

    3

--- lesson3/t1/Program.cs
+++ lesson3/t1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿/*1. Напишите программу, которая принимает на вход
+координаты точки (X и Y), причём X ≠ 0 или Y ≠ 0 и
+выдаёт номер четверти плоскости, в которой находится
+эта точка.*/
+
+void Quarters(int x, int y)
+{
+    if (x == 0 || y == 0)
+        Console.WriteLine("x=0,y=0");
+    else if (x > 0 && y > 0)
+        Console.WriteLine("1");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("2");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("3");
+    else if (x > 0 && y < 0)
+        Console.WriteLine("4");
+    
+}
+Quarters(5,-3);
