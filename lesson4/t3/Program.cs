cd08ec11570303b5ed80fad9ab2127d44dd0c5bb
Julechka <julialobankova123@gmail.com>
2022-08-05 01:17:32 +0300

    zadacha3

---
+++ lesson4/t3/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+int number(int A)
+{
+    int mult = 1;
+    for(int i = 1; i <= A; i++)
+        mult = mult * i;
+    return mult;
+}
+Console.WriteLine(number(4));
+
