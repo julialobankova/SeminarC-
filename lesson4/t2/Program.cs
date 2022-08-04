3e31cf0cb46aebe6082162d22d22828dbddf8eac
Julechka <julialobankova123@gmail.com>
2022-08-05 00:57:11 +0300

    zadacha2

---
+++ lesson4/t2/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+void number (string A)
+{
+        Console.WriteLine(A.Length);
+} 
+number(Convert.ToString(Console.ReadLine()));
