1e5328d4f07e6d9b5fb8a539945ca3648965144d
Julechka <julialobankova123@gmail.com>
2022-08-05 23:13:41 +0300

    zadacha43

---
+++ lesson4/t4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая выводит
+массив из 8 элементов, заполненный нулями и
+единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+void RandomArray(int num)
+{
+    int[] array = new int[num];
+    for(int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write(array[i]);
+    } 
+}
+RandomArray(15);
