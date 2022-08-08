37dc02e5ab00b7d9fb9372fe78ffffa0177ea0d2
Julechka <julialobankova123@gmail.com>
2022-08-09 02:28:44 +0300

    dz1

---
+++ Homework/HW5/t1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в
+массиве.
+[345, 897, 568, 234] -> 2*/
+
+void ArrayPrint(int[] array2)
+{
+    for(int i=0; i < array2.Length; i++)
+        Console.Write($"{array2[i]} "); 
+        Console.Write("\n");  
+}
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        arr [i] = new Random().Next(from,to);
+    }
+    return arr;
+}
+void even(int[] array)
+{
+    int sum = 0;
+     for(int i = 0; i < array.Length; i++)
+     {
+        if(array[i] % 2 == 0)
+        {
+            sum = sum+1;   
+        }
+        
+     }
+     Console.WriteLine(sum);
+}
+int [] array1 = MassNums(5,100,1000);
+ArrayPrint(array1);
+even(array1);
