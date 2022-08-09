2f21eae5eae4702322301499fb2a272031d27861
Julechka <julialobankova123@gmail.com>
2022-08-09 03:33:09 +0300

    papki

---
+++ Lesson1-5/lesson1z23/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Write a weekday number: ");
+int a = int.Parse(Console.ReadLine());
+if (a == 1)
+    Console.WriteLine("пн");
+else if (a == 2)
+    Console.WriteLine("вт");
+else if (a == 3)
+    Console.WriteLine("ср");
+else if (a == 4)
+    Console.WriteLine("чт");
+else if (a == 5)
+    Console.WriteLine("пт");
+else if (a == 6)
+    Console.WriteLine("сб");
+else if (a == 7)
+    Console.WriteLine("вс");
+else 
+    Console.WriteLine("Ошибка ввода");
+
