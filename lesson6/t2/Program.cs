30039e89d9722321e8a8926e6115702a494fa0ed
Julechka <julialobankova123@gmail.com>
2022-08-14 15:58:40 +0300

    les62

--- lesson6/t2/Program.cs
+++ lesson6/t2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /*Напишите программу, которая принимает на вход три
+числа и проверяет, может ли существовать треугольник с сторонами
+такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника
+меньше суммы двух других сторон.*/
+void triangle(int num1, int num2, int num3)
+{
+    if (num1<num2+num3 && num2<num1+num3 && num3<num2+num1)
+    {
+        Console.WriteLine("Ура у вас треугольник");
+    }
+    else
+    {
+        Console.WriteLine("Увы, треугольника не состряпать");
+    }
+}
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+triangle(Convert.ToInt32(a),Convert.ToInt32(b),Convert.ToInt32(c));
