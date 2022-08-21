415f99b77a901b1dd0038b2e596dce0152015920
Julechka <julialobankova123@gmail.com>
2022-08-22 01:53:34 +0300

    DZ2

--- Homework/hw8/t2/Program.cs
+++ Homework/hw8/t2/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте прямоугольный двумерный массив. 
+Напишите программу, которая будет находить строку
+с наименьшей суммой элементов.*/
+
+void Print(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int colomnSize = arr.GetLength(1);
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < colomnSize; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int colomn, int from, int to)
+{
+    int[,] arr = new int[row, colomn];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < colomn; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+
+    }
+    return arr;
+}
+
+
+
+    void RowWithMinSumElement(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+    int[] sumRow= new int[rowSize];
+    int minRow=0;
+    
+    for(int i=0; i<rowSize; i++)
+    {
+        for(int j=0; j<columnSize; j++)
+        {
+            sumRow[i]+=arr[i,j];
+        }
+        
+        if (sumRow[i]<sumRow[minRow])
+        {
+            minRow=i;
+        }
+            
+    }
+    Console.WriteLine();
+    Console.WriteLine($" Номер строки с наименьшей суммой элементов: {minRow+1} строка");
+}
+
+    int[,] arr1 = MassNums(4, 4, 1, 10);
+    Print(arr1);
+    RowWithMinSumElement(arr1);
+
