41f6eb5e42cfb5aeb3876eb786929e64f8f4d8fc
Julechka <julialobankova123@gmail.com>
2022-08-24 19:55:38 +0300

    dz91

---
+++ Homework/HW9/t1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задайте значения M и N. Напишите программу, 
+которая выведет все чётные натуральные числа в промежутке от M до N.*/
+
+
+
+void EvenIntNumbers(int min, int max)
+{
+    max-=max%2;
+    if (max<min) return;
+    EvenIntNumbers(min,max-2);
+    Console.Write($"{max}, ");
+}
+
+EvenIntNumbers(1,10);
---
+++ Homework/HW9/t2/Program.cs
@@ -0,0 +1,10 @@
+﻿ /*Задайте значения M и N. Напишите программу,
+ которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+int SumMinMax(int min, int max)
+{
+    if(min==max) return max;
+    return SumMinMax(min, max-1) +max;
+
+}
+Console.WriteLine(SumMinMax(1,50));
